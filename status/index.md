7f31ec9177ae63eeab39eeb3ec2258315304d639
hb <hb@aroma-project.org>
2017-06-22 13:16:45 -0700

    STATUS: Add Cluster Heartbeat badges

--- status/index.md
+++ status/index.md
@@ -10,8 +10,8 @@ WARNING: do _not_ update this file; it is automatically generated from index.md.
 
 ## Total CPU Load
 
-<img src=https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_year.gif alt=Total CPU load the last 12 months>
-<img src=https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_month.gif alt=Total CPU load the last 31 days>
-<img src=https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_week.gif alt=Total CPU load the last 7 days>
-<img src=https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_day.gif alt=Total CPU load the last 24 hours>
-<img src=https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_hour.gif alt=Total CPU load the last one hour>
+<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_year.gif" alt="Total CPU load the last 12 months">
+<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_month.gif" alt="Total CPU load the last 31 days">
+<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_week.gif" alt="Total CPU load the last 7 days">
+<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_day.gif" alt="Total CPU load the last 24 hours">
+<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_hour.gif" alt="Total CPU load the last one hour">
--- status/index.md.sh
+++ status/index.md.sh
@@ -19,11 +19,8 @@ echo "# Cluster Usage"
 echo
 echo "## Total CPU Load"
 echo
-echo "<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_year.gif" alt="Total CPU load the last 12 months">"
-echo "<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_month.gif" alt="Total CPU load the last 31 days">"
-echo "<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_week.gif" alt="Total CPU load the last 7 days">"
-echo "<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_day.gif" alt="Total CPU load the last 24 hours">"
-echo "<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_hour.gif" alt="Total CPU load the last one hour">"
-
-
-
+echo '<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_year.gif" alt="Total CPU load the last 12 months">'
+echo '<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_month.gif" alt="Total CPU load the last 31 days">'
+echo '<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_week.gif" alt="Total CPU load the last 7 days">'
+echo '<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_day.gif" alt="Total CPU load the last 24 hours">'
+echo '<img src="https://raw.githubusercontent.com/UCSF-TI/TIPCC-slash/master/TIPCC/docs/status/figures/cpu_load_last_hour.gif" alt="Total CPU load the last one hour">'
